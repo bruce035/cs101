b2f6d16d6783ae48723172da6145295db14f6ae6
bruce035 <113400422+bruce035@users.noreply.github.com>
2023-01-04 11:30:06 +0800

    Update main1.c

--- Hw8/main1.c
+++ Hw8/main1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int main()
+{
+    int rows = 10;
+    for (int row = 1; row <= rows; row++) {
+        
+        for (int i = 1; i <= rows - row; i++) {
+            printf(" ");
+        }
+        
+        for (int i = 1; i <= 2 * row - 1; i++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
